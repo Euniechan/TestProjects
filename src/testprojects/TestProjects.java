0ec8425650af3acbb9e9c65320b890cb23d3f18a
Euniechan <euniejen04@gmail.com>
2024-01-26 16:46:10 +0800

    modify testprojects.java

--- src/testprojects/TestProjects.java
+++ src/testprojects/TestProjects.java
@@ -14,7 +14,9 @@ public class TestProjects {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.print("Hello World");
+        System.out.print("Hello Earth");
     }
     
 }
